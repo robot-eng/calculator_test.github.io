dfddd76d944a547f1b6140791582aea1a91a7fe7
Watcahanan <79349006+robot-eng@users.noreply.github.com>
2023-03-29 17:44:58 +0700

    Update script.js

--- login_Register/assets/script.js
+++ login_Register/assets/script.js
@@ -1,10 +1,15 @@
 const logregBox = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+const exitmenu = document.querySelector('.exit');
+const menu = document.querySelector('#check:checked ~ ul');
 registerLink.addEventListener('click', () =>{
     logregBox.classList.add('active')
 });
 
 loginLink.addEventListener('click', () =>{
     logregBox.classList.remove('active')
+});
+exitmenu.addEventListener('click', () =>{
+    menu.classList.remove('active')
 });
